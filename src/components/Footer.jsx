0ad808a57432de20525b6d5dfc44b8faea80a391
DESKTOP-049POIU\USER <shishirjaman.sj@gmail.com>
2026-01-17 03:15:52 -0500

    added copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,7 @@ export default function Footer(props){
                 <div className="bgGradient"></div>
                 <h1>APOD Project</h1>
                 <h2>{data?.title}</h2>
+                <p>Copyright &copy; Shishir Zaman - A demo react project using Nasa Apod API.</p>
             </div>
             <button onClick={handleToggleModal}>
                 <i className="fa-solid fa-circle-info"></i>
